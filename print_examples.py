5ceb9ff1dcef8c61ebe6cf02e1104d4a70a021a5
mateo <mateocc2013@gmail.com>
2023-01-26 10:59:47 +0100

    class example

---
+++ print_examples.py
@@ -0,0 +1,5 @@
+print ("This is a test", end="I want this at the end")
+print ("This is another")
+print ("this", "can", "be", "written", "like", "this")
+#This is not a useful comment
+print("this", "can", "be", "written", "like", "this", sep="!b ?")
